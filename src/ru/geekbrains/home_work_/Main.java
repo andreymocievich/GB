8bf15af87153c51547c784da96cbaebf9934eb47
andreymocievich <andrey-mocievich@yandex.ru>
2020-07-03 22:23:07 +0300

    test

---
+++ src/ru/geekbrains/home_work_/Main.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.home_work_;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = -100;
+        short b = 22456;
+        int c = 300000;
+        long d = 100L;
+        float e = 12.34F;
+        double f = 123.45D;
+        char j = '*';
+        boolean i = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(j);
+        System.out.println(i);
+    }
+}
